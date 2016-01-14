2d0145fb8cc076a6c356d62b0c355590eaf42002
danny2303 <danzo2303@gmail.com>
2016-01-14 19:45:54 +0000

    Create DungeonsAndDragons.java

---
+++ DungeonsAndDragons.java
@@ -0,0 +1,11 @@
+package dungeonsanddragons;
+
+public class DungeonsAndDragons {
+
+    static Menu menu = new Menu();
+    static Function function = new Function();
+
+    public static void main(String[] args) {
+        menu.game.mainMenu();
+    }
+}
